f83d8b6e85abeb7a14b3c77cc6c8da7666b54068
Viral Panchal <viralpanchal1999@gmail.com>
2022-10-14 14:52:47 -0400

    Q2 done

--- HW05/Q2/Q2.m
+++ HW05/Q2/Q2.m
@@ -1,19 +1,20 @@
-close all
+close all 
 clear all
 clc
 
 L = 3048e-3;
-x = 0:0.001:L;
+x =  0:0.001:L;
 E = 200e9;
 I = (50.8e-3)*((100e-3)^3)/12;
 L1 = 914e-3;
 L3 = 914e-3;
-L2 = L-L1-L3;
+L2 = L - L1 - L3;
 
-P2 = -1000;
-M2 = 0;
-P3 = -1000;
-M3 = 0;
+q = -1640;
+P2 = q*L2/2;
+M2 = q*L2^2/12;
+P3 = q*L2/2;
+M3 = -q*L2^2/12;
 
 K = @(LL) E*I/LL^3 * [12 6*LL -12 6*LL; 6*LL 4*LL^2 -6*LL 2*LL^2; -12 -6*LL 12 -6*LL; 6*LL 2*LL^2 -6*LL 4*LL^2]
 K1 = K(L1)
@@ -25,14 +26,10 @@ MainK(1:4,1:4) = K1;
 MainK(3:6,3:6) = MainK(3:6,3:6) + K2;
 MainK(5:8,5:8) = MainK(5:8,5:8) + K3
 
-% Calculation forces and displacements:
-
 Force_vec = [P2 M2 P3 M3]';
 Displacement_vec = [0; 0; MainK(3:6,3:6)\Force_vec; 0;0]
 
 Force_vec = MainK*Displacement_vec
-
-% Caculating the points on the equation
 
 V = @(V1,tet1,V2,tet2,Lin,zet)(1-3*zet.*zet+2*zet.*zet.*zet)*V1 + Lin*(zet-2*zet.*zet+zet.*zet.*zet)*tet1+(3*zet.*zet-2*zet.*zet.*zet)*V2+Lin*(zet.*zet.*zet-zet.*zet)*tet2;
 
